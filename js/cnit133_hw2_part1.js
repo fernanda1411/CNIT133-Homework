d6abb19088bc82cb7ddeea384f0475d143c64710
Fernanda Silva <fernanda.rodrigues.cdc@gmail.com>
2016-09-12 23:11:23 -0700

    Setup for Homework 3

---
+++ js/cnit133_hw2_part1.js
@@ -0,0 +1,13 @@
+console.log("cnit133_hw2_part1");
+
+var myString;
+
+// Applyring requirements to myString
+myString = "<span style = 'font-family: Times New Roman; color: red; font-weight:bold; margin-right:.5em;'>ab</span>";
+
+myString += "<span style = 'font-family: Arial; color: blue; font-weight:bold; margin-right:.5em;'>12</span>";
+
+myString += "<span style = 'font-family: Impact; font-style: italic; color: green; font-weight:bold'>34</span>";
+
+
+document.write(myString);
